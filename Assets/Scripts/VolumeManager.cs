040b3f5ded4064d22d2c8494b6b635b12b156219
VicCTS <victordm.cts@gmail.com>
2023-03-31 12:49:45 +0200

    Finished

---
+++ Assets/Scripts/VolumeManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class VolumeManager : MonoBehaviour
+{
+    [SerializeField] AudioMixer mixer;
+    [SerializeField] string volumeParameter;
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] Toggle muteToggle;
+    [SerializeField] bool muted;
+
+    void Awake()
+    {
+        volumeSlider.onValueChanged.AddListener(ChangeVolume); 
+        muteToggle.onValueChanged.AddListener(Mute);
+    }
+
+    void Start()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat(volumeParameter, volumeSlider.maxValue);
+
+        string muteValue = PlayerPrefs.GetString(volumeParameter + "Mute", "False");
+
+        if(muteValue == "False")
+        {
+            muted = false;
+        }
+        else if(muteValue == "True")
+        {
+            muted = true;
+        }
+
+        muteToggle.isOn = !muted;
+    }
+
+    void OnDisable()
+    {
+        PlayerPrefs.SetFloat(volumeParameter, volumeSlider.value);
+        PlayerPrefs.SetString(volumeParameter + "Mute", muted.ToString());
+    }
+
+    public void ChangeVolume(float value)
+    {
+        mixer.SetFloat(volumeParameter, value);
+        muteToggle.isOn = volumeSlider.value > volumeSlider.minValue;
+    }
+
+    void Mute(bool soundEnabled)
+    {
+        if(soundEnabled)
+        {
+            float lastVolume = PlayerPrefs.GetFloat(volumeParameter, volumeSlider.maxValue);
+            mixer.SetFloat(volumeParameter, lastVolume);
+            muted = false;
+        }
+        else
+        {
+            PlayerPrefs.SetFloat(volumeParameter, volumeSlider.value);
+            mixer.SetFloat(volumeParameter, volumeSlider.minValue);
+            muted = true;
+        }
+    }
+}
